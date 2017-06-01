e6d03b745a3ebc1f57cd3e0354dee211fadb7d65
Juan-Alberto Hernández-Martínez <juan.alberto.hernandez.martinez@gmail.com>
2017-05-31 21:35:45 -0500

    Adding a test to create a List of Java files by using the File class.

--- src/DytecodeTest.java
+++ src/DytecodeTest.java
@@ -1,3 +1,5 @@
+import beans.Util;
+
 import java.io.File;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
@@ -8,11 +10,31 @@ public class DytecodeTest {
     public static void main(String... args) throws Exception {
         Dytecode dytecode = new Dytecode();
 
-        // Compiling and loading multiple classes in a specific directory
-        List<File> listFiles = dytecode.getJavaFiles("/home/unmasked_00/Documentos/Programacion/Pruebas/app");
+        // Compiling and loading multiple classes in a specific directory by using the
+        // getJavaFiles()
+        List<File> listFiles = Util.getJavaFiles("/home/unmasked_00/Documentos/Programacion/Pruebas/app");
         List<List<Class<?>>> compiledClasses = dytecode.compileAndLoad(listFiles);
 
         for(List<Class<?>> list : compiledClasses){
+            for(Class cls : list) {
+                System.out.println(cls.getName());
+                for(Method m : cls.getDeclaredMethods()){
+                    System.out.println(m.getName());
+                }
+            }
+        }
+
+        // Compiling and loading multiple classes in a specific directory by using the
+        // File class
+        File f1 = new File("/home/unmasked_00/Documentos/Programacion/Pruebas/app/A.java");
+        File f2 = new File("/home/unmasked_00/Documentos/Programacion/Pruebas/app/B.java");
+        List<File> files = new ArrayList<>();
+        files.add(f1);
+        files.add(f2);
+
+        List<List<Class<?>>> compiledClasses2 = dytecode.compileAndLoad(files);
+
+        for(List<Class<?>> list : compiledClasses2){
             for(Class cls : list) {
                 System.out.println(cls.getName());
                 for(Method m : cls.getDeclaredMethods()){
